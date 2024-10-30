21124451454958cdca90393af906f79a7d048332
bhuvan <bhuvanannappa@gmail.com>
2024-10-30 19:10:29 +0530

    added a username  and password prompt

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
-//localStorage.clear();
+alert(
+  "Admin Username: admin@me.com\n" +
+    "Password: 123\n\n" +
+    "First Employee Username: employee1@example.com\n" +
+    "Password: 123\n\n" +
+    "Note: The rest of the employee usernames follow the same pattern (e.g., employee2@example.com, employee3@example.com) and can be adjusted by changing the numbers."
+);
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <App />
