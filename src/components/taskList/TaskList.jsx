b25655b8fe1171a962a643c167fbfde7ef48302f
bhuvan <bhuvanannappa@gmail.com>
2024-10-30 18:33:03 +0530

    a

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import AcceptTask from "./AcceptTask";
 import NewTask from "./NewTask";
 import CompleteTask from "./CompleteTask";
-import FailedTask from "./FailedTask";
 
 const TaskList = ({ data }) => {
   return (
@@ -19,9 +18,6 @@ const TaskList = ({ data }) => {
         }
         if (elem.completed) {
           return <CompleteTask key={idx} data={elem} />;
-        }
-        if (elem.failed) {
-          return <FailedTask key={idx} data={elem} />;
         }
       })}
     </div>
